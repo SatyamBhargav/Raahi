f700097403e0f543fb5ec9de3c093dc0295a4c53
Satyam Bhargav <satyambhargav17@gmail.com>
2023-12-07 13:56:36 +0530

    added login & sign up pages

---
+++ lib/screens/createaccountscreen.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:raahi/screens/loginscreen.dart';
+import 'package:raahi/widget/frostedglass.dart';
+
+class CreateAccount extends StatefulWidget {
+  const CreateAccount({super.key});
+
+  @override
+  State<CreateAccount> createState() => _CreateAccountState();
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  // final _emailController = TextEditingController();
+  // final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  var _emailController = '';
+  var _passwordController = '';
+
+  void saveDetails() {
+    // final isValid = _formKey.currentState!.validate();
+    // // if (!isValid || !_isLogin && _selectedImage == null) {
+    // //   return;
+    // // }
+    // _formKey.currentState!.save();
+
+    // _firebase.createUserWithEmailAndPassword(
+    //     email: _emailController, password: _passwordController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/login.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: FrostGlass(
+            height: 500.0,
+            width: 380.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Create New Account',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontSize: 30),
+                ),
+                const Text(
+                  'Please fill in the form to continue',
+                  style: TextStyle(color: Colors.black),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Form(
+                      child: Column(
+                    children: [
+                      TextFormField(
+                          cursorColor: Colors.white,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              contentPadding:
+                                  const EdgeInsets.only(left: 30, top: 40),
+                              hintText: 'Full Name',
+                              hintStyle: const TextStyle(
+                                  fontSize: 15, color: Colors.white30),
+                              filled: true,
+                              fillColor: const Color(0xff00303D),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(25)))),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          onSaved: (newValue) {
+                            _emailController = newValue!;
+                          },
+                          cursorColor: Colors.white,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              contentPadding:
+                                  const EdgeInsets.only(left: 30, top: 40),
+                              hintText: 'Email Address',
+                              hintStyle: const TextStyle(
+                                  fontSize: 15, color: Colors.white30),
+                              filled: true,
+                              fillColor: const Color(0xff00303D),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(25)))),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                          onSaved: (newValue) {
+                            _passwordController = newValue!;
+                          },
+                          cursorColor: Colors.white,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              contentPadding:
+                                  const EdgeInsets.only(left: 30, top: 40),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(
+                                  fontSize: 15, color: Colors.white30),
+                              filled: true,
+                              fillColor: const Color(0xff00303D),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(25)))),
+                      const SizedBox(height: 50),
+                      ElevatedButton(
+                          style: const ButtonStyle(
+                              minimumSize:
+                                  MaterialStatePropertyAll(Size.fromHeight(50)),
+                              backgroundColor:
+                                  MaterialStatePropertyAll(Color(0xff5566ff))),
+                          onPressed: () {
+                            saveDetails();
+                          },
+                          child: const Text(
+                            'Sign In',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't Have An Account?",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => const LoginScreen()));
+                              },
+                              child: const Text(
+                                'Sign In',
+                                style: TextStyle(color: Colors.blueAccent),
+                              ))
+                        ],
+                      ),
+                    ],
+                  )),
+                ),
+                // const Row(
+                //   crossAxisAlignment: CrossAxisAlignment.center,
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   mainAxisSize: MainAxisSize.min,
+                //   children: [
+                //     Divider(
+                //       color: Colors.green,
+                //       endIndent: 200,
+                //       thickness: 50,
+                //     ),
+                //     Text('Or'),
+                //     Divider(
+                //       endIndent: 200,
+                //       thickness: 50,
+                //     ),
+                //   ],
+                // )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/loginscreen.dart
@@ -0,0 +1,158 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:raahi/screens/createaccountscreen.dart';
+import 'package:raahi/widget/frostedglass.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final _usernameController = TextEditingController();
+    final _passwordController = TextEditingController();
+
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/login.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: FrostGlass(
+            height: 500.0,
+            width: 380.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(
+                  'Welcome Back!',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium!
+                      .copyWith(fontSize: 30),
+                ),
+                const Text(
+                  'Please sign in to your account',
+                  style: TextStyle(color: Colors.black),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Form(
+                      autovalidateMode: AutovalidateMode.always,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                              controller: _usernameController,
+                              cursorColor: Colors.white,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                  contentPadding:
+                                      const EdgeInsets.only(left: 30, top: 40),
+                                  hintText: 'Username',
+                                  hintStyle: const TextStyle(
+                                      fontSize: 15, color: Colors.white30),
+                                  filled: true,
+                                  fillColor: const Color(0xff00303D),
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide.none,
+                                      borderRadius:
+                                          BorderRadius.circular(25)))),
+                          const SizedBox(height: 10),
+                          TextFormField(
+                              controller: _passwordController,
+                              cursorColor: Colors.white,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                  contentPadding:
+                                      const EdgeInsets.only(left: 30, top: 40),
+                                  hintText: 'Password',
+                                  hintStyle: const TextStyle(
+                                      fontSize: 15, color: Colors.white30),
+                                  filled: true,
+                                  fillColor: const Color(0xff00303D),
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide.none,
+                                      borderRadius:
+                                          BorderRadius.circular(25)))),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Forgot Password?',
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                          ),
+                          const ElevatedButton(
+                              style: ButtonStyle(
+                                  minimumSize: MaterialStatePropertyAll(
+                                      Size.fromHeight(50)),
+                                  backgroundColor: MaterialStatePropertyAll(
+                                      Color(0xff5566ff))),
+                              onPressed: null,
+                              child: Text(
+                                'Sign In',
+                                style: TextStyle(color: Colors.white),
+                              )),
+                        ],
+                      )),
+                ),
+                const SizedBox(height: 20),
+                // GestureDetector(
+                //   onTap: () => _handelGoogleSignIn(),
+                //   child: Padding(
+                //     padding: const EdgeInsets.symmetric(horizontal: 40),
+                //     child: Container(
+                //       padding: const EdgeInsets.symmetric(vertical: 5),
+                //       decoration: BoxDecoration(
+                //           color: Colors.white,
+                //           borderRadius: BorderRadius.circular(20)),
+                //       child: Row(
+                //         mainAxisAlignment: MainAxisAlignment.center,
+                //         children: [
+                //           Image.asset('images/google.png', height: 40),
+                //           const SizedBox(width: 15),
+                //           const Text('Sign in with Google')
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't Have An Account?",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const CreateAccount()));
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(color: Colors.blueAccent),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
